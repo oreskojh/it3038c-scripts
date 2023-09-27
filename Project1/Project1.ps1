92596f7a838bba7c6d1506f3ad8443a7009ae21f
oreskojh <74508224+oreskojh@users.noreply.github.com>
2023-09-27 14:34:37 -0400

    Add files via upload

---
+++ Project1/Project1.ps1
@@ -0,0 +1,11 @@
+﻿#Read-Host's purpose is to account for different drive names that can occur which can vary depending on device setup.
+$driveName = Read-Host "Please enter your drive name (i.e. C): "
+$driveSelection = Get-PSDrive $driveName 
+$usedStorageSpace = $driveSelection.used
+$freeStorageSpace = $driveSelection.free
+
+#Rounding is used to shorten the results due to the numerous decimal numbers the initial calculations contain
+$freeStorageSpaceGB = [math]::round(($freeStorageSpace / 1GB), 2)
+$freeStorageSpacePercent = [math]::round($freeStorageSpace / ($freeStorageSpace + $usedStorageSpace) * 100, 2)
+
+Write-Host "On Drive $driveName there is currently $freeStorageSpacePercent% or $freeStorageSpaceGB GB of storage left."
