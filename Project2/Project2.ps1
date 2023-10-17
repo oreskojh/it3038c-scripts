91dd77acedaf5cde5fc40a83b666bffac2cae4a7
oreskojh <74508224+oreskojh@users.noreply.github.com>
2023-10-17 19:34:26 -0400

    Add files via upload

---
+++ Project2/Project2.ps1
@@ -0,0 +1,19 @@
+﻿#Read-Host's purpose is to account for different drive names that can occur which can vary depending on device setup.
+$driveName = Read-Host "Please enter your drive name (i.e. C) "
+
+#Creates the full root file path to be added in $fileSizeCheck
+$filePath = $driveName + ":\"
+
+#A variable is used to creates a hash table from the Get-Child results so that the file length can be adjusted to a more appropriate format.
+$fileSizeCheck = Get-ChildItem -Path $filePath -Recurse -ErrorAction SilentlyContinue | Where-Object {$_.length -gt 200MB} | Select-Object FullName, length | Sort-Object length
+foreach ($file in $fileSizeCheck)
+{
+    $fileSize = [math]::round(($file.Length/1MB),2)
+    Write-Host "File Name: "$file.FullName
+    Write-Host "File Size: $fileSize MB"
+}
+
+Write-Host "`nThe script has concluded."
+
+#This Read-Host is used to keep the PowerShell window from closing immediately after running the script.
+Read-Host "Please hit Enter to exit"
