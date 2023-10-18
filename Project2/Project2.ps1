515d3bc8480c0604b53296c3c6883245ff1799a2
oreskojh <74508224+oreskojh@users.noreply.github.com>
2023-10-17 21:31:47 -0400

    Update Project2.ps1 Comments

--- Project2/Project2.ps1
+++ Project2/Project2.ps1
@@ -4,8 +4,10 @@ $driveName = Read-Host "Please enter your drive name (i.e. C) "
 #Creates the full root file path to be added in $fileSizeCheck
 $filePath = $driveName + ":\"
 
-#A variable is used to creates a hash table from the Get-Child results so that the file length can be adjusted to a more appropriate format.
+#A variable is used to create a hash table from the Get-Child results so that the file length can be adjusted to a more appropriate format.
 $fileSizeCheck = Get-ChildItem -Path $filePath -Recurse -ErrorAction SilentlyContinue | Where-Object {$_.length -gt 200MB} | Select-Object FullName, length | Sort-Object length
+
+#Goes through the hash table and converts the length value from bytes to MB and outputs the results of the Get-ChildItem.
 foreach ($file in $fileSizeCheck)
 {
     $fileSize = [math]::round(($file.Length/1MB),2)
